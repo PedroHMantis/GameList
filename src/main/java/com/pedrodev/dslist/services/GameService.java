80d7ab99d36d18c6ac4e9b6773d765337ed624f3
PedroHMantis <gtx1050pd@gmail.com>
2025-08-21 13:38:26 -0300

    implement SQL queries using Spring Data JPA and Projections

--- src/main/java/com/pedrodev/dslist/services/GameService.java
+++ src/main/java/com/pedrodev/dslist/services/GameService.java
@@ -4,6 +4,7 @@ package com.pedrodev.dslist.services;
 import com.pedrodev.dslist.dto.GameDTO;
 import com.pedrodev.dslist.dto.GameMinDTO;
 import com.pedrodev.dslist.entities.Game;
+import com.pedrodev.dslist.projections.GameMinProjection;
 import com.pedrodev.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -35,5 +36,11 @@ public class GameService {
 
         return dto;
 
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
